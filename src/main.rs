19045f52a634b7b1ebc7c4fb75c8cfdc32251200
ramelen <ramelen@proton.me>
2024-04-17 17:42:28 -0700

    added list selection

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,8 @@ use crossterm::{
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
-use ratatui::{
-    prelude::{CrosstermBackend, Stylize, Terminal},
-    widgets::Paragraph,
-};
+use ratatui::prelude::{CrosstermBackend, Terminal};
+use ratatui::{prelude::*, widgets::*};
 use std::io::{stdout, Result};
 
 fn main() -> Result<()> {
@@ -14,21 +12,51 @@ fn main() -> Result<()> {
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
+
+    let todos = vec![
+        "Help menu",
+        "Adding items",
+        "Editing items",
+        "Removing items",
+        "Completing items",
+    ];
+
+    let mut state = ListState::default();
+    state.select(Some(0));
+
     loop {
         terminal.draw(|frame| {
             let area = frame.size();
-            frame.render_widget(
-                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                    .white()
-                    .on_blue(),
-                area,
-            );
+            let list = List::new(todos.clone())
+                .block(
+                    Block::default()
+                        .title("<Press q to quit>")
+                        .borders(Borders::NONE),
+                )
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().fg(Color::Cyan));
+            frame.render_stateful_widget(list, area, &mut state);
         })?;
 
         if event::poll(std::time::Duration::from_millis(16))? {
             if let event::Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                    break;
+                if key.kind == KeyEventKind::Press {
+                    match key.code {
+                        KeyCode::Char('q') => break,
+                        KeyCode::Char('k') => {
+                            let selected = state.selected().unwrap_or_default();
+                            state.select(Some(if selected == 0 { 0 } else { selected - 1 }))
+                        }
+                        KeyCode::Char('j') => {
+                            let selected = state.selected().unwrap_or_default();
+                            state.select(Some(if selected >= todos.len() - 1 {
+                                todos.len() - 1
+                            } else {
+                                selected + 1
+                            }))
+                        }
+                        _ => {}
+                    }
                 }
             }
         }
