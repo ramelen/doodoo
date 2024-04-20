2c5a208c6851c9237a0102d5254d2e84d3ada764
ramelen <ramelen@proton.me>
2024-04-19 23:50:25 -0700

    terminal now exits properly when panicing

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,20 @@ use crossterm::{
 };
 use ratatui::prelude::{CrosstermBackend, Terminal};
 use ratatui::{prelude::*, widgets::*};
-use std::io::{stdout, Result};
+use std::io::{self, stdout, Result};
 
 fn main() -> Result<()> {
     stdout().execute(EnterAlternateScreen)?;
     enable_raw_mode()?;
     let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
     terminal.clear()?;
+
+    let original_hook = std::panic::take_hook();
+    std::panic::set_hook(Box::new(move |panic| {
+        disable_raw_mode().unwrap();
+        crossterm::execute!(io::stdout(), LeaveAlternateScreen).unwrap();
+        original_hook(panic);
+    }));
 
     let mut todos = vec![
         "Help menu",
@@ -21,8 +28,8 @@ fn main() -> Result<()> {
         "Completing items",
     ];
 
-    let mut state = ListState::default();
-    state.select(Some(0));
+    let mut list_state = ListState::default();
+    list_state.select(Some(0));
 
     loop {
         terminal.draw(|frame| {
@@ -35,7 +42,7 @@ fn main() -> Result<()> {
                 )
                 .style(Style::default().fg(Color::White))
                 .highlight_style(Style::default().fg(Color::Cyan));
-            frame.render_stateful_widget(list, area, &mut state);
+            frame.render_stateful_widget(list, area, &mut list_state);
         })?;
 
         if event::poll(std::time::Duration::from_millis(16))? {
@@ -44,18 +51,18 @@ fn main() -> Result<()> {
                     match key.code {
                         KeyCode::Char('q') => break,
                         KeyCode::Char('k') => {
-                            select_previous(&mut state);
+                            select_previous(&mut list_state);
                         }
                         KeyCode::Char('j') => {
-                            select_next(&mut state, &todos);
+                            select_next(&mut list_state, &todos);
                         }
-                        KeyCode::Char('d') if state.selected().is_none() => {}
+                        KeyCode::Char('d') if list_state.selected().is_none() => {}
                         KeyCode::Char('d') => {
-                            todos.remove(state.selected().unwrap_or(todos.len() + 1));
+                            todos.remove(list_state.selected().unwrap_or(todos.len() + 1));
                             if todos.len() != 0 {
-                                select_previous(&mut state);
+                                select_previous(&mut list_state);
                             } else {
-                                state.select(None);
+                                list_state.select(None);
                             }
                         }
                         _ => {}
@@ -70,9 +77,9 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn select_next(state: &mut ListState, todos: &Vec<&str>) {
-    if let Some(selection) = state.selected() {
-        state.select(Some(if selection >= todos.len() - 1 {
+fn select_next(list_state: &mut ListState, todos: &Vec<&str>) {
+    if let Some(selection) = list_state.selected() {
+        list_state.select(Some(if selection >= todos.len() - 1 {
             todos.len() - 1
         } else {
             selection + 1
@@ -80,9 +87,9 @@ fn select_next(state: &mut ListState, todos: &Vec<&str>) {
     }
 }
 
-fn select_previous(state: &mut ListState) {
-    if let Some(selection) = state.selected() {
-        state.select(Some(if selection == 0 { 0 } else { selection - 1 }))
+fn select_previous(list_stace: &mut ListState) {
+    if let Some(selection) = list_stace.selected() {
+        list_stace.select(Some(if selection == 0 { 0 } else { selection - 1 }))
     } else {
         return;
     }
